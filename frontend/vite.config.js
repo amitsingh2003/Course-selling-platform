49219ad761cb8922ca6e264c3d246801f50cf570
amitsingh2003 <amitsingh2003plwl@gmail.com>
2025-01-13 13:14:26 +0530

    vite-congif

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,4 +7,8 @@ export default defineConfig({
   server: {
     port: 3000, // Replace 3000 with your desired port number
   },
-})
+  build: {
+    outDir: 'dist', // The default is 'dist', but you can customize it if needed
+    sourcemap: true, // Optional: Helps debugging in production
+  },
+});
